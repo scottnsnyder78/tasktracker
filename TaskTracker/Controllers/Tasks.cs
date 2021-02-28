f7b5a46807ba5cf0fdc50c39cd610e1baea441d7
scott <scottnsnyder78@gmail.com>
2021-02-27 22:22:38 -0500

    initial

---
+++ TaskTracker/Controllers/Tasks.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using AutoFixture;
+using Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskTracker.Controllers
+{
+    
+    [ApiController]
+    [Route("[controller]")]
+    public class TasksController :  ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Tasks> Get()
+        {
+            var fixture = new Fixture();
+            return fixture.Create<IEnumerable<Tasks>>();
+        }
+    }
+}
